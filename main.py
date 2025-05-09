62e3d959febdb0f59890a4bff2af9e41b12964c0
Bektur <muratov_b@auca.kg>
2025-05-09 10:35:25 +0000

    rollback

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://s-ranking.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
